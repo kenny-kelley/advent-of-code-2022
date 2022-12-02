15add399463e996f3d60dd13c8f889d62a2543b4
Kenny Kelley <16879853+kenny-kelley@users.noreply.github.com>
2022-12-01 22:58:31 -0500

    Working solution for Day 1

--- day_01/solution.rb
+++ day_01/solution.rb
@@ -1,6 +1,23 @@
 #!/usr/bin/env ruby
 
 if __FILE__ == $0
-    # TODO: Actually solve the problem
-    print "Hewwo, worwd!"
+
+    # Read in the number groupings and split on each double newline
+    number_groupings = File.read(__dir__ + "/input.txt").split("\n\n")
+
+    # Declare an array to save the number grouping sums in
+    number_grouping_sums = []
+
+    # Iterate over each number grouping
+    number_groupings.each {
+        |number_grouping|
+
+        # Split the number grouping on each newline, convert them all to integers, sum those
+        #   integers, then append that sum to the array of number grouping sums
+        number_grouping_sums.append(number_grouping.split("\n").map(&:to_i).sum)
+    }
+
+    # Print out the maximum of the number grouping sums
+    puts number_grouping_sums.max
+
 end
