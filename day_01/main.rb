19c64f7ebaa04d5eacbedb8d1d78b05ce22a3441
Kenny Kelley <16879853+kenny-kelley@users.noreply.github.com>
2022-12-02 00:07:19 -0500

    Refactoring some and adding a solution for Day 1, Part 2

--- day_01/main.rb
+++ day_01/main.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
-if __FILE__ == $0
+# Returns an array of the sums of each number grouping in input.txt
+def get_number_grouping_sums()
 
     # Read in the number groupings and split on each double newline
     number_groupings = File.read("#{__dir__}/input.txt").split("\n\n")
@@ -14,11 +15,47 @@ if __FILE__ == $0
         # Split the number grouping on each newline, convert them all to integers, sum those
         #   integers, then append that sum to the array of number grouping sums
         number_grouping_sums.append(number_grouping.split("\n").map(&:to_i).sum)
-
     end
 
-    # Print out the maximum of the number grouping sums
-    puts "Solution:"
+    return number_grouping_sums
+end
+
+# Returns an array of the {x} highest values in an array of integers
+def get_highest_values(values, x)
+
+    # Initialize an array to keep track of the {x} highest values
+    highest_values = []
+
+    # Iterate over ever each {value}
+    values.each do |value|
+
+        # If we haven't saved {x} values yet, store this {value}
+        if highest_values.size < x
+            highest_values.append(value)
+
+        # If this {value} is greater than the minimum of the highest values encountered so far,
+        #   delete the first instance of the minimum and append this {value}
+        elsif value > highest_values.min
+            highest_values.delete_at(highest_values.find_index(highest_values.min))
+            highest_values.append(value)
+        end
+    end
+
+    return highest_values
+end
+
+if __FILE__ == $0
+
+    # Get the sums of each number grouping
+    number_grouping_sums = get_number_grouping_sums()
+
+    # Print the max of those sums
+    puts "### Part One Solution ###"
     puts number_grouping_sums.max
 
+    puts "\n"
+
+    # Print the sum of the highest three of those sums
+    puts "### Part Two Solution ###"
+    puts get_highest_values(number_grouping_sums, 3).sum
 end
