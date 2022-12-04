f3488f8544096cfe4418e2004d1f297027ff1ec7
Kenny Kelley <16879853+kenny-kelley@users.noreply.github.com>
2022-12-04 00:08:30 -0500

    Stubbing out Day 4 input and solutions

---
+++ day_04/main.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+# Advent of Code 2022, Day 04
+# Author: Kenny Kelley
+# Date: 2022-12-04
+
+
+# Do it
+if __FILE__ == $0
+    puts "### Advent of Code 2022, Day 04 ###"
+    puts "\n"
+
+    puts "### Part 1 Solution ###"
+    puts "TODO"
+
+    puts "\n"
+
+    puts "### Part 2 Solution ###"
+    puts "TODO"
+end
