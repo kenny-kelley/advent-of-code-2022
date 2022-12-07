142abd4a260f8a8d486e1b445fbb6278fa065c66
Kenny Kelley <16879853+kenny-kelley@users.noreply.github.com>
2022-12-07 01:33:00 -0500

    Stubbing out input and solutions for Day 7

---
+++ day_07/main.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+# Advent of Code 2022, Day 07
+# Author: Kenny Kelley
+# Date: 2022-12-07
+
+
+# Do the thing
+if __FILE__ == $0
+    puts "### Advent of Code 2022, Day 07 ###"
+    puts "\n"
+
+    puts "### Part 1 Solution ###"
+    puts "TODO"
+    puts "\n"
+
+    puts "### Part 2 Solution ###"
+    puts "TODO"
+    puts "\n"
+end
