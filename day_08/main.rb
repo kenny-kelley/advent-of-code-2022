911776d965f3a12aa3ad377aa1496861ac6dc6d4
Kenny Kelley <16879853+kenny-kelley@users.noreply.github.com>
2022-12-10 23:20:13 -0500

    Refactored the solution to Day 8, Part 1 into a class

--- day_08/main.rb
+++ day_08/main.rb
@@ -9,6 +9,72 @@
 require "set"
 
 
+# A class that represents the puzzle state
+class PuzzleState
+    def initialize(tree_grid)
+        @tree_grid = tree_grid
+        @observed_trees = Set.new
+        @tallest_tree_in_view = nil
+    end
+
+    attr_reader :observed_trees
+
+    def observe_tree(m, n)
+        @observed_trees.add("#{m},#{n}")
+        @tallest_tree_in_view = @tree_grid[m][n]
+    end
+
+    def attempt_to_observe_tree(m, n)
+        if @tree_grid[m][n] > @tallest_tree_in_view
+            self.observe_tree(m, n)
+        end
+    end
+
+    def look_from_left_and_right
+
+        # Iterate over the rows
+        (0...@tree_grid.length).each do |m|
+
+            # Iterate over the columns (look from the left side of the grid)
+            self.observe_tree(m, 0)
+            (0...@tree_grid[m].length).each do |n|
+                self.attempt_to_observe_tree(m, n)
+            end
+
+            # Iterate over the columns in reverse (look from the right side of the grid)
+            self.observe_tree(m, @tree_grid[m].length - 1)
+            (0...@tree_grid[m].length).reverse_each do |n|
+                self.attempt_to_observe_tree(m, n)
+            end
+        end
+    end
+
+    def look_from_and_top_and_bottom
+
+        # Iterate over the columns
+        (0...@tree_grid[0].length).each do |n|
+
+            # Iterate over the rows (look from the top side of the grid)
+            self.observe_tree(0, n)
+            (0...@tree_grid.length).each do |m|
+                self.attempt_to_observe_tree(m, n)
+            end
+
+            # # Iterate over the rows in reverse (look from the bottom side of the grid)
+            self.observe_tree(@tree_grid.length - 1, n)
+            (0...@tree_grid.length).reverse_each do |m|
+                self.attempt_to_observe_tree(m, n)
+            end
+        end
+    end
+
+    def look_from_all_sides
+        self.look_from_left_and_right
+        self.look_from_and_top_and_bottom
+    end
+end
+
+
 # Returns the puzzle input, which is an MxN matrix of integers (meaning M rows and N columns)
 def get_tree_grid
     result = Array.new
@@ -19,76 +85,11 @@ def get_tree_grid
 end
 
 
-# TODO: Write a method description
-def update_observed_trees(observed_trees, n, m)
-    observed_trees.add("#{m},#{n}")
-end
-
-
-# TODO: Write a method description
-def look_from_left_and_right(tree_grid, observed_trees)
-
-    # Iterate over the rows
-    (0...tree_grid.length).each do |m|
-
-        # Iterate over the columns (look from the left side of the grid)
-        tallest_tree_so_far = tree_grid[m][0]
-        update_observed_trees(observed_trees, m, 0)
-        (0...tree_grid[m].length).each do |n|
-            if tree_grid[m][n] > tallest_tree_so_far
-                tallest_tree_so_far = tree_grid[m][n]
-                update_observed_trees(observed_trees, m, n)
-            end
-        end
-
-        # Iterate over the columns in reverse (look from the right side of the grid)
-        tallest_tree_so_far = tree_grid[m][tree_grid[m].length - 1]
-        update_observed_trees(observed_trees, m, tree_grid[m].length - 1)
-        (0...tree_grid[m].length).reverse_each do |n|
-            if tree_grid[m][n] > tallest_tree_so_far
-                tallest_tree_so_far = tree_grid[m][n]
-                update_observed_trees(observed_trees, m, n)
-            end
-        end
-    end
-end
-
-
-# TODO: Write a method description
-def look_from_and_top_and_bottom(tree_grid, observed_trees)
-
-    # Iterate over the columns
-    (0...tree_grid[0].length).each do |n|
-
-        # Iterate over the rows (look from the top side of the grid)
-        tallest_tree_so_far = tree_grid[0][n]
-        update_observed_trees(observed_trees, 0, n)
-        (0...tree_grid.length).each do |m|
-            if tree_grid[m][n] > tallest_tree_so_far
-                tallest_tree_so_far = tree_grid[m][n]
-                update_observed_trees(observed_trees, m, n)
-            end
-        end
-
-        # # Iterate over the rows in reverse (look from the bottom side of the grid)
-        tallest_tree_so_far = tree_grid[tree_grid.length - 1][n]
-        update_observed_trees(observed_trees, tree_grid.length - 1, n)
-        (0...tree_grid.length).reverse_each do |m|
-            if tree_grid[m][n] > tallest_tree_so_far
-                tallest_tree_so_far = tree_grid[m][n]
-                update_observed_trees(observed_trees, m, n)
-            end
-        end
-    end
-end
-
-
-# TODO: Write a method description
+# Finds the number of trees visible from outside the grid
 def find_part_one_solution(tree_grid)
-    observed_trees = Set.new
-    look_from_left_and_right(tree_grid, observed_trees)
-    look_from_and_top_and_bottom(tree_grid, observed_trees)
-    return observed_trees.size
+    puzzle_state = PuzzleState.new(tree_grid)
+    puzzle_state.look_from_all_sides
+    return puzzle_state.observed_trees.size
 end
 
 
